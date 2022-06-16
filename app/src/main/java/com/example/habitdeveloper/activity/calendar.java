d069ab36ad10a5674b45e486a4d5717a59ff8c4f
Forlorin <fuskying@outlook.com>
2022-06-16 10:57:42 +0800

    日历+颜色
    新建日历activity
    加入应该使用的颜色

---
+++ app/src/main/java/com/example/habitdeveloper/activity/calendar.java
@@ -0,0 +1,16 @@
+package com.example.habitdeveloper.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.habitdeveloper.R;
+
+public class calendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
